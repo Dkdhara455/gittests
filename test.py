7ae21bd4f7cae23bc796961bd30ebe87ba083777
Deepak <dharadeepakkumar2000@gmail.com>
2025-08-04 11:33:20 +0530

    modify test.py

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
 def greet():
     print("Hello, Deepak!")
+greet()
+# Output: Hello, Deepak!
+
+
+# Function with Return Value
+def square(x):
+    return x * x
+result = square(4)
+print(result)
+# Output: 16
+
+# Default Parameters
+def welcome(name="Guest"):
+    print("Welcome,", name)
+
+welcome()
+welcome("Deepak")
+# Output:
+# Welcome, Guest
+# Welcome, Deepak
